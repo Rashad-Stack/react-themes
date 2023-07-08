4b587b7a0c608b59c0d7e10fdf440781c892e674
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-07-08 16:22:40 +0600

    theme configure added more automated featured

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,33 @@ import "./App.css";
 
 function App() {
   const [dark, setDark] = useState<boolean>(false);
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const prefersLight = window.matchMedia(
+      "(prefers-color-scheme: light)"
+    ).matches;
+    const prefersNotSet = window.matchMedia(
+      "(prefers-color-scheme: no-preference)"
+    ).matches;
+
+    // Media Hook to check what theme user prefers
+    if (prefersDark) {
+      setDark(true);
+    }
+
+    if (prefersLight) {
+      setDark(false);
+    }
+
+    if (prefersNotSet) {
+      document.querySelector("html")?.classList.remove("dark");
+      document.querySelector("html")?.classList.remove("light");
+    }
+  }, []);
+
   useEffect(() => {
     if (dark) {
       document.querySelector("html")?.classList.add("dark");
